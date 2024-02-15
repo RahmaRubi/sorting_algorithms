967703668ec86901000bf908ecbbcd4cfbac70e6
Omar Essam <omar161157@eng.psu.edu.eg>
2024-02-15 22:43:06 +0200

    New commit

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,21 +1,5 @@
 #include <stdio.h>
-#include <stdlib.h>
-
-/**
-* print_arr - prints an array
-* @arr: array
-* @size: array size
-*/
-
-
-void print_arr(int *arr, int size)
-{
-	int i = 0;
-
-	for (i = 0; i < size; i++)
-		printf("%d ", arr[i]);
-	printf("\n");
-}
+#include "sort.h"
 
 /**
 * selection_sort - sorts an array using selection sort
@@ -30,19 +14,19 @@ void selection_sort(int *array, size_t size)
 	for (i = 0; i < (int) size; i++)
 	{
 		int *old_num = &array[i];
-		int *new_num;
+		int *new_num = &array[i];
 		int j = i;
 
 		for (j = i + 1; j < (int) size; j++)
-			if(*new_num < *old_num)
+			if (array[j] < *new_num)
 				new_num = &array[j];
 
 		if (old_num != new_num)
 		{
-			int *temp = old_num;
-			old_num = new_num;
-			new_num = temp;
-			print_arr(array, size);
+			int temp = *old_num;
+			*old_num = *new_num;
+			*new_num = temp;
+			print_array(array, size);
 		}
 	}
 }
