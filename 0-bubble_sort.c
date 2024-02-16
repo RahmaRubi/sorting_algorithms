80ae355b5eb79fa4367a903769d65638874f3251
Rahma Rubi <rahmaelroby47@gmail.com>
2024-02-16 07:21:34 -0800

    exactly like a bubble

---
+++ 0-bubble_sort.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "sort.h"
+
+void bubble_sort(int *array, size_t size)
+{
+	size_t i, j, temp, swap;
+
+	swap = 0;
+	if (!array || size == 0)
+		return;
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < size - 1 - i; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				swap = 1;
+				temp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = temp;
+				print_array(array, size);
+			}
+		}
+		if (!swap)
+			break;
+	}
+}
+
