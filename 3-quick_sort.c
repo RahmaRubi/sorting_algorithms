0d75629f20ebe07d9ffd65ffc612d4fba82068b1
Omar Essam <omar161157@eng.psu.edu.eg>
2024-02-16 02:26:43 +0200

    New commit

---
+++ 3-quick_sort.c
@@ -0,0 +1,77 @@
+#include "sort.h"
+#include <stdio.h>
+
+/**
+* swap - swaps two integers
+* @a: first integer
+* @b: second integer
+*/
+void swap(int *a, int *b)
+{
+	int temp = *a;
+	*a = *b;
+	*b = temp;
+}
+
+/**
+* partition - partitions the array around the pivot
+* @array: array
+* @l: start index
+* @r: end index
+* @size: array size
+* Return: pivot position
+*/
+int partition(int *array, int l, int r, size_t size)
+{
+	int pivot = array[r];
+	int smaller = l - 1;
+	int i;
+
+	for (i = l; i < r; i++)
+	{
+		if (array[i] < pivot)
+		{
+			smaller++;
+			if (i != smaller)
+				swap(&array[smaller], &array[i]), print_array(array, size);
+		}
+	}
+
+	swap(&array[smaller + 1], &array[r]);
+
+	return (smaller + 1);
+}
+
+
+/**
+* quick_sort_rec - recursive function for quick sort
+* @array: array
+* @l: left index
+* @r: right index
+* @size: size
+*/
+void quick_sort_rec(int *array, int l, int r, size_t size)
+{
+	if (l < r)
+	{
+		int pivot = partition(array, l, r, size);
+
+		quick_sort_rec(array, l, pivot - 1, size);
+		quick_sort_rec(array, pivot + 1, r, size);
+	}
+}
+
+/**
+* quick_sort - array sorting
+* @array: array
+* @size: array size
+*/
+
+void quick_sort(int *array, size_t size)
+{
+	if (size <= 1)
+		return;
+
+	quick_sort_rec(array, 0, size - 1, size);
+}
+
--- sort.h
+++ sort.h
@@ -4,5 +4,7 @@
 
 void print_array(const int *array, size_t size);
 void selection_sort(int *array, size_t size);
+void quick_sort(int *array, size_t size);
+
 
 #endif
